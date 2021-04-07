396fc0b4aa5d5c4c6ce2d5629adfec3215b72a81
justDude <test>
2021-04-08 00:54:22 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simbirsoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr_ru = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
+            var url = new Uri("https://www.simbirsoft.com/");
+            string fileName = "html.txt";
+            new WebClient().DownloadFile(url, fileName);
+            var item = new Dictionary<string, int>();
+            
+            using(StreamReader reader = new StreamReader(fileName))
+            {
+                string[] file = reader.ReadToEnd().Split(new string[] { " ", "<", ">", "\n", "/", "" }, StringSplitOptions.RemoveEmptyEntries);
+                file = file.Select(x => x.Replace(" ", "")).ToArray();
+                
+                foreach(var i in file)
+                {
+                    var lower = string.Concat(i.Select(x => char.ToLower(x)));
+
+                    if (lower.IndexOfAny(arr_ru) != -1)
+                    {
+                        if (item.ContainsKey(lower))
+                            item[lower]++;
+                        else
+                            item.Add(lower, 1);
+                    }
+
+                }
+                StreamWriter writer = new StreamWriter("test.txt");
+                foreach (var a in item)
+                {
+                    writer.Write($"{a.Key} {a.Value}\n");
+                }
+                
+            }
+        }
+    }
+}
