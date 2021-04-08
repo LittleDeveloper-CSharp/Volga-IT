446bb3db1c429a10e5c62e4a75e7ce85114aaf9f
justDude <test>
2021-04-08 23:02:09 +0400

    Прикрутила базу

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using Simbirsoft.Parser;
 using Simbirsoft.CounterWords;
 using System.Diagnostics;
 using System.Reflection;
-using System.Threading;
+using Simbirsoft.Models;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Simbirsoft
 {
@@ -11,48 +13,78 @@ namespace Simbirsoft
     {
         static void Main(string[] args)
         {
-            ParseHtml parse = new ParseHtml();
+            ApplicationContext context = new ApplicationContext();
+            context.UniqueWords.Load();
 
-            Console.Write("Введите URL: ");
-            var url = Console.ReadLine();
+            Console.Write("Что сделать? (1-вывести из базы, 2-ввести URL): ");
+            var answer = int.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-
-            Console.Write("Введите кол-во вхождений слова: ");
-            var countText = Console.ReadLine();
-
-            try
+            if (answer == 1)
             {
-                if (int.TryParse(countText, out int count))
+                Console.WriteLine("\n\nСписок слов:\n");
+                foreach (var word in context.UniqueWords.Local.ToList())
+                    Console.WriteLine($"{word.Word} - {word.Count}");
+            }
+            else if(answer == 2)
+            {
+
+                ParseHtml parse = new ParseHtml();
+
+                Console.Write("Введите URL: ");
+                var url = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.Write("Введите кол-во вхождений слова: ");
+                var countText = Console.ReadLine();
+
+                try
                 {
-                    //"https://www.simbirsoft.com/"
-                    var streamHtml = parse.GetHtmlStream(url);
-
-                    var parsedHtml = parse.ParseHtmlOfStream(streamHtml);
-
-                    UniqueWords uniqueWords = new UniqueWords();
-
-                    var clearArray = uniqueWords.ClearArrayWithWord(parsedHtml);
-
-                    var dictionaryUniqueWord = uniqueWords.CountUniqueWord(clearArray);
-
-                    foreach (var keyValue in dictionaryUniqueWord)
+                    if (int.TryParse(countText, out int count))
                     {
-                        if(keyValue.Value >= count)
-                            Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+                        //"https://www.simbirsoft.com/"
+                        var streamHtml = parse.GetHtmlStream(url);
+
+                        var parsedHtml = parse.ParseHtmlOfStream(streamHtml);
+
+                        UniqueWords uniqueWords = new UniqueWords();
+
+                        var clearArray = uniqueWords.ClearArrayWithWord(parsedHtml);
+
+                        var dictionaryUniqueWord = uniqueWords.CountUniqueWord(clearArray);
+
+                        foreach (var keyValue in dictionaryUniqueWord)
+                        {
+                            if (keyValue.Value >= count)
+                            {
+                                Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+                                var item = context.UniqueWords.Local.FirstOrDefault(x => x.Word == keyValue.Key && x.Url == url);
+                                if (item == null)
+                                {
+                                    var lastItem = context.UniqueWords.Local.LastOrDefault();
+                                    var lastIndex = lastItem == null ? 1 : lastItem.IdWord + 1;
+                                    context.UniqueWords.Add(new UniqueWord { IdWord = lastIndex, Url = url, Word = keyValue.Key, Count = keyValue.Value });
+                                }
+                                else
+                                    item.Count += keyValue.Value;
+
+                            }
+                        }
+                        context.SaveChanges();
+                        Console.WriteLine("\nНажмите на любую кнопку");
                     }
+                    else
+                        throw new Exception("Некорректное число");
                 }
-                else
-                    throw new Exception("Некорректное число");
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("\nНажмите любую кнопку...");
-                Console.ReadKey();
-                Process.Start(Assembly.GetExecutingAssembly().Location);
-                Environment.Exit(0);
-            }
+            Console.WriteLine("\nНажмите любую кнопку...");
+            Console.ReadKey();
+            Process.Start(Assembly.GetExecutingAssembly().Location);
+            Environment.Exit(0);
         }
     }
 }
